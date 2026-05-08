e9173b8520b3ec16ff8706c42fdccbc64728b30b
Howard M. Lewis Ship <hlship@gmail.com>
2026-05-08 10:02:50 -0700

    aamachine 1.0.0
    Installs prebuilt OSX Arm64
    Now include aamrun

--- Formula/aamachine.rb
+++ Formula/aamachine.rb
@@ -1,13 +1,12 @@
 class Aamachine < Formula
   desc "Support for the Aa-machine virtual machine for Interactive Fiction"
-  homepage "https://github.com/dialog-IF/dialog"
-  url "https://hd0.linusakesson.net/files/aamachine-0.5.1.zip"
-  sha256 "8357c201e45628b2b0a654f16d4cb4c3f47f7c9e7f1f09e059c9b66916d8fcb5"
+  homepage "https://github.com/Dialog-IF/aamachine"
+  url "https://github.com/Dialog-IF/aamachine/releases/download/release-1.0.0/aamachine-1.0.0.zip"
+  sha256 "9b35da6b3c8c98e5bff3dfadd3ad1fca0c1b020ffb02b46c30be94858ac487f7"
 
   def install
-    chdir "src" do
-      system "make"
-      bin.install "aambundle", "aamshow"
+    chdir "prebuilt/macos-arm64" do
+      bin.install "aambundle", "aamshow", "aamrun"
     end
     prefix.install "readme.txt", "license.txt"
   end

1239e32a497c5118c824d6e404e88a53367a8297
Howard M. Lewis Ship <hlship@gmail.com>
2026-05-13 10:37:07 -0700

    Correct URL

--- Formula/aamachine.rb
+++ Formula/aamachine.rb
@@ -1,7 +1,7 @@
 class Aamachine < Formula
   desc "Support for the Aa-machine virtual machine for Interactive Fiction"
   homepage "https://github.com/Dialog-IF/aamachine"
-  url "https://github.com/Dialog-IF/aamachine/releases/download/untagged-2c4b688ec0abcf0940f3/aamachine-1.0.1.zip"
+  url "https://github.com/Dialog-IF/aamachine/releases/download/release-1.0.1/aamachine-1.0.1.zip"
   sha256 "2413c14130e3bc1462cb4ef35abd75dbfe3a2a770a6308160671dec3e440990d"
 
   def install

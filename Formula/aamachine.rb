b6994856066f561ca26c5cf56b758bb85a3e0586
Howard M. Lewis Ship <hlship@gmail.com>
2026-05-13 10:31:58 -0700

    aamachine 1.0.1

--- Formula/aamachine.rb
+++ Formula/aamachine.rb
@@ -1,11 +1,11 @@
 class Aamachine < Formula
   desc "Support for the Aa-machine virtual machine for Interactive Fiction"
   homepage "https://github.com/Dialog-IF/aamachine"
-  url "https://github.com/Dialog-IF/aamachine/releases/download/release-1.0.0/aamachine-1.0.0.zip"
-  sha256 "9b35da6b3c8c98e5bff3dfadd3ad1fca0c1b020ffb02b46c30be94858ac487f7"
+  url "https://github.com/Dialog-IF/aamachine/releases/download/untagged-2c4b688ec0abcf0940f3/aamachine-1.0.1.zip"
+  sha256 "2413c14130e3bc1462cb4ef35abd75dbfe3a2a770a6308160671dec3e440990d"
 
   def install
-    chdir "prebuilt/macos-arm64" do
+    chdir "prebuilt/macos-universal" do
       bin.install "aambundle", "aamshow", "aamrun"
     end
     prefix.install "readme.txt", "license.txt"
